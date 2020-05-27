c413397b18fa14f6ce217298e9fad09e26197a25
Benoît Hubert <benoithubert@gmail.com>
2020-05-27 13:59:00 +0200

    Updates

---
+++ precache-manifest.62532de6cd5289d94a44d2f65b533b15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d09198d59f14d586f8165432360a54",
+    "url": "/book-meeting-app/index.html"
+  },
+  {
+    "revision": "41d8fcb97659297f176e",
+    "url": "/book-meeting-app/static/css/2.c92686f8.chunk.css"
+  },
+  {
+    "revision": "c14b582249a79c5622c8",
+    "url": "/book-meeting-app/static/css/main.06025fb1.chunk.css"
+  },
+  {
+    "revision": "41d8fcb97659297f176e",
+    "url": "/book-meeting-app/static/js/2.df9bb108.chunk.js"
+  },
+  {
+    "revision": "003c96490c7aaee62db21e3b5f7ebec6",
+    "url": "/book-meeting-app/static/js/2.df9bb108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14b582249a79c5622c8",
+    "url": "/book-meeting-app/static/js/main.cd46a540.chunk.js"
+  },
+  {
+    "revision": "50c356d057bcb64ceffb",
+    "url": "/book-meeting-app/static/js/runtime-main.318e37f6.js"
+  }
+]);
